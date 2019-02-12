39a62eba204cb2fb1ceb092dd4c75f4ee161307d
Burak Karahan <burak.karahan@mail.ru>
2019-02-12 23:02:59 +0300

    Second Commit

---
+++ demo/data/models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.db import models
+from django.conf import settings
+
+# DataSet Object
+class DataSet(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    content = models.ImageField(upload_to='%Y/%m/%d/', max_length=100)
+    release_datetime = models.DateTimeField(auto_now_add=True)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s %s %s %s" % (self.uuid, self.name, self.content, self.default)
