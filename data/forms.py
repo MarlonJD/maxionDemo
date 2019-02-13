fafa9deac2acb4db863ee115e4689a5ef6364440
Burak Karahan <burak.karahan@mail.ru>
2019-02-13 13:43:01 +0300

    Modify add page

--- data/forms.py
+++ data/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import DataSet
 
 class DocumentForm(forms.ModelForm):
+    name = forms.CharField(label="Ad Soyad")
+    content = forms.ImageField(label="Veri/Fotoğraf")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'label': 'Your name'})
+        self.fields['content'].widget.attrs.update({'class': 'form-control-file'})
     class Meta:
         model = DataSet
         fields = ('name', 'content', )
